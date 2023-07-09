d0b3b6752453ae4c65a4ed1b0d46fc91b35e4ba9
Phodal Huang <h@phodal.com>
2023-07-09 10:15:09 +0800

    docs: add sample for delete

--- src/main/java/cc/unitmesh/untitled/demo/controller/BlogController.java
+++ src/main/java/cc/unitmesh/untitled/demo/controller/BlogController.java
@@ -7,10 +7,14 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/blog")
 public class BlogController {
     BlogService blogService;
 
@@ -29,4 +33,6 @@ public class BlogController {
     public List<BlogPost> getBlog() {
         return blogService.getAllBlogPosts();
     }
+
+    // delete blog
 }
