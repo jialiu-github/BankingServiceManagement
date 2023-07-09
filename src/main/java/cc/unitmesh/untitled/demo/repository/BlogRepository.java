a6ce33fcfb276fb90787be6d21053fc1d6e07a28
Phodal Huang <h@phodal.com>
2023-07-09 13:50:09 +0800

    refactor: clean for code

--- src/main/java/cc/unitmesh/untitled/demo/repository/BlogRepository.java
+++ src/main/java/cc/unitmesh/untitled/demo/repository/BlogRepository.java
@@ -1,10 +1,10 @@
 package cc.unitmesh.untitled.demo.repository;
 
 import cc.unitmesh.untitled.demo.entity.BlogPost;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BlogRepository extends JpaRepository<BlogPost, Long> {
+public interface BlogRepository extends CrudRepository<BlogPost, Long> {
 
 }
--- src/main/java/cc/unitmesh/untitled/demo/service/BlogService.java
+++ src/main/java/cc/unitmesh/untitled/demo/service/BlogService.java
@@ -24,4 +24,6 @@ public class BlogService {
         // todo
         return new ArrayList<>();
     }
+
+    // delete blog
 }
