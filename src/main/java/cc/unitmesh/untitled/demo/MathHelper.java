e70a791e588ab87ce57463b9e147a1477b0b511c
Phodal Huang <h@phodal.com>
2024-02-22 10:56:00 +0800

    refactor(math): improve readability of insurance calculation logic
    Simplify and clarify the insurance calculation logic in MathHelper class for better understanding and maintainability.

--- src/main/java/cc/unitmesh/untitled/demo/MathHelper.java
+++ src/main/java/cc/unitmesh/untitled/demo/MathHelper.java
@@ -1,16 +1,6 @@
 package cc.unitmesh.untitled.demo;
 
 public class MathHelper {
-    /**
-     * Calculates the insurance amount based on the given income.
-     *
-     * @param income the income of the individual for whom insurance amount needs to be calculated
-     * @return the insurance amount calculated based on the income:
-     * - If income is less than or equal to 10000, insurance amount is income * 0.365
-     * - If income is between 10001 and 30000, insurance amount is (income - 10000) * 0.2 + 35600
-     * - If income is between 30001 and 60000, insurance amount is (income - 30000) * 0.1 + 76500
-     * - If income is greater than 60000, insurance amount is (income - 60000) * 0.02 + 105600
-     */
     public double calculateInsurance(double income) {
         if (income <= 10000) {
             return income * 0.365;
