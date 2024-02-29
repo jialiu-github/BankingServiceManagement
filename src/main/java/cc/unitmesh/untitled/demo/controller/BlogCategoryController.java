e50d9db9ff1ee51d393f59ffabfba1ce9f65fd75
Phodal Huang <h@phodal.com>
2024-02-29 09:14:53 +0800

    chore(controller): add bank account management
    Add functionality for bank account management in the BlogCategoryController.
    
    Fixes: devti://story/github/1

--- src/main/java/cc/unitmesh/untitled/demo/controller/BlogCategoryController.java
+++ src/main/java/cc/unitmesh/untitled/demo/controller/BlogCategoryController.java
@@ -5,4 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BlogCategoryController {
     // devti://story/github/1
+
+    //    银行账户管理
+
+    // Close a bank account
 }
