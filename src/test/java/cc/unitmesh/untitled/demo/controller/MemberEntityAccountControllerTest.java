125271f589dab6daeaceb7d4aab28f246feaa899
Jia Liu <jialiu@thoughtworks.com>
2024-04-22 18:23:12 +0800

    step two add framework info into prompt

--- src/test/java/cc/unitmesh/untitled/demo/controller/MemberEntityAccountControllerTest.java
+++ src/test/java/cc/unitmesh/untitled/demo/controller/MemberEntityAccountControllerTest.java
@@ -4,38 +4,50 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 import cc.unitmesh.untitled.demo.service.MemberEntityAccountService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
-public class PaymentLimitControllerTest {
+@WebMvcTest(MemberController.class)
+public class MemberControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @MockBean
     private MemberEntityAccountService memberEntityAccountService;
 
-    private PaymentLimitController paymentLimitController;
-
-    @BeforeEach
-    public void setup() {
-        MockitoAnnotations.openMocks(this);
-        paymentLimitController = new PaymentLimitController(memberEntityAccountService);
-    }
-
     @Test
-    public void testGetPaymentLimit() {
+    public void should_return_payment_limit_for_valid_account_id() throws Exception {
         // Given
         String accountId = "123";
         String expectedPaymentLimit = "1000";
+        when(memberEntityAccountService.getPaymentLimitBy(accountId)).thenReturn(expectedPaymentLimit);
 
         // When
-        when(memberEntityAccountService.getPaymentLimitBy(accountId)).thenReturn(expectedPaymentLimit);
-        String actualPaymentLimit = paymentLimitController.getPaymentLimit(accountId);
+        mockMvc.perform(MockMvcRequestBuilders.get("/" + accountId + "/paymentLimit"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(expectedPaymentLimit));
 
         // Then
-        assertEquals(expectedPaymentLimit, actualPaymentLimit);
+        assertEquals(expectedPaymentLimit, memberEntityAccountService.getPaymentLimitBy(accountId));
+    }
+
+    @Test
+    public void should_return_not_found_for_invalid_account_id() throws Exception {
+        // Given
+        String accountId = "456";
+        when(memberEntityAccountService.getPaymentLimitBy(accountId)).thenReturn(null);
+
+        // When
+        mockMvc.perform(MockMvcRequestBuilders.get("/" + accountId + "/paymentLimit"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+
+        // Then
+        assertEquals(null, memberEntityAccountService.getPaymentLimitBy(accountId));
     }
 }
