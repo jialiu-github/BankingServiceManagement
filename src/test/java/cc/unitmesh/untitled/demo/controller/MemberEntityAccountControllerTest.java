73cb83b76fe62aaf2ca9c998302bd164e2b122f3
Jia Liu <jialiu@thoughtworks.com>
2024-04-22 18:44:41 +0800

    v2: update payment limit

--- src/test/java/cc/unitmesh/untitled/demo/controller/MemberEntityAccountControllerTest.java
+++ src/test/java/cc/unitmesh/untitled/demo/controller/MemberEntityAccountControllerTest.java
@@ -1,33 +1,58 @@
 package cc.unitmesh.untitled.demo.controller;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import cc.unitmesh.untitled.demo.dto.AccountPaymentLimitDto;
+import cc.unitmesh.untitled.demo.service.MemberEntityAccountService;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@ExtendWith(MockitoExtension.class)
-public class AccountControllerTest {
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PaymentLimitControllerTest {
 
-    @InjectMocks
-    private AccountController accountController;
+    @Autowired
+    private MockMvc mockMvc;
 
-    @Mock
+    @MockBean
     private MemberEntityAccountService memberEntityAccountService;
 
     @Test
-    public void testUpdatePaymentLimit() {
+    public void should_update_payment_limit_successfully() throws Exception {
         // Given
-        AccountPaymentLimitDto accountPaymentLimitDto = new AccountPaymentLimitDto();
-        accountPaymentLimitDto.setAccountId(123);
-        accountPaymentLimitDto.setPaymentLimit(1000);
+        AccountPaymentLimitDto accountPaymentLimitDto = new AccountPaymentLimitDto("12345", 1000);
 
         // When
-        accountController.updatePaymentLimit(accountPaymentLimitDto);
-
+        mockMvc.perform(MockMvcRequestBuilders.put("/paymentLimit")
+                .contentType("application/json")
+                .content("{\"accountId\":\"12345\",\"paymentLimit\":1000}")
+        )
         // Then
-        verify(memberEntityAccountService).updateAccount(123, 1000);
+        .andExpect(MockMvcResultMatchers.status().isNoContent());
+
+        // Verify that the service method was called with the correct parameters
+        verify(memberEntityAccountService).updateAccount("12345", 1000);
+    }
+
+    @Test
+    public void should_fail_to_update_payment_limit_invalid_input() throws Exception {
+        // Given
+        AccountPaymentLimitDto accountPaymentLimitDto = new AccountPaymentLimitDto(null, 1000);
+
+        // When
+        mockMvc.perform(MockMvcRequestBuilders.put("/paymentLimit")
+                .contentType("application/json")
+                .content("{\"accountId\":null,\"paymentLimit\":1000}")
+        )
+        // Then
+        .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+        // Verify that the service method was not called
+        verify(memberEntityAccountService, never()).updateAccount(anyString(), anyInt());
     }
 }
